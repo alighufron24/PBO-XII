7f4fdaec907387fb1067e6ecd361d11b394b0f54
Ali Ghufron <alighufron.ag33@gmail.com>
2021-08-03 15:03:18 +0700

    php

--- php-oop-master/detail-warga.php
+++ php-oop-master/detail-warga.php
@@ -43,7 +43,7 @@ $data=$Db->get_by_id($idwarga);
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="Template/index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
