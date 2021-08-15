600f57dfd04ce76263d0a7a543c6eebb027ae035
Ali Ghufron <alighufron.ag33@gmail.com>
2021-08-15 14:48:35 +0700

    finish

--- blog-alighufron/resources/views/blog/edit.blade.php
+++ blog-alighufron/resources/views/blog/edit.blade.php
@@ -75,22 +75,6 @@
 
     
 
-    <!-- Bootstrap core JavaScript-->
-    <!-- <script src="../../Template/vendor/jquery/jquery.min.js"></script> -->
-    <!-- <script src="../../Template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script> -->
-
-    <!-- Core plugin JavaScript-->
-    <script src="../../Template/vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="../../Template/js/sb-admin-2.min.js"></script>
-
-    <!-- Page level plugins -->
-    <script src="../../Template/vendor/datatables/jquery.dataTables.min.js"></script>
-    <script src="../../Template/vendor/datatables/dataTables.bootstrap4.min.js"></script>
-
-    <!-- Page level custom scripts -->
-    <script src="../../Template/s/demo/datatables-demo.js"></script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
--- blog-alighufron/resources/views/template.blade.php
+++ blog-alighufron/resources/views/template.blade.php
@@ -194,7 +194,7 @@
     <!-- End of Page Wrapper -->
 
     <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
+    <a class="scroll-to-top rounded-circle" href="#page-top">
         <i class="fas fa-angle-up"></i>
     </a>
     
